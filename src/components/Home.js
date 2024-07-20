e3c846571c35131d27c0748cc9b181a42f082063
Saiprakash2115 <saiprakash.98483@gmail.com>
2024-07-20 20:13:06 +0530

    update

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import { useLocation, useNavigate } from "react-router-dom";
+import {useNavigate } from "react-router-dom";
 import { useState } from "react";
 
 function Home() {
@@ -7,12 +7,18 @@ function Home() {
     const  readingtext =(value)=>{
         setTitle(value)
     }
-
+    const onSubmitBtn = (e) => {
+      e.preventDefault();  
+              setTimeout(() => {
+                navigate(`/Wishes/${title}`);
+              }, 3000);
+            }
+  
   return (
     <div className="p-5" style={{backgroundColor:"yellowgreen"}}>
     <div className="container-fluid my-5" >
      <div className="row">
-      <div className="col-md-6 text-center">
+      <form className="col-md-6 text-center" onSubmit={onSubmitBtn} >
         <h4 className="my-3">Please Enter Your Cute Name</h4>
         <input 
         className="my-3"
@@ -22,9 +28,9 @@ function Home() {
         <br/>
         <button 
         className="btn btn-light"
-        onClick={()=>navigate(`/Wishes/${title}`)}
+        type="submit"
         ><b>Enter</b></button>
-      </div>
+      </form>
      </div>
     </div>
     </div>
